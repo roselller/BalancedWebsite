19a96af6965fcd1b783ac1626dd643a4011d3a85
nehCnitsuJ <s10222137@connect.np.edu.sg>
2022-02-11 18:11:27 +0800

    Edited index.html, index.js, index.css

---
+++ script/index.js
@@ -0,0 +1,8 @@
+/*Popup function for adding of notes*/
+function openFunction() {
+    document.getElementById("add").style.display = "block";
+  }
+  
+  function closeFunction() {
+    document.getElementById("add").style.display = "none";
+  }
