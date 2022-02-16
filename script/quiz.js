9c0406898deb2f2352318eabaf04c952aaa8869d
Armirola Roseller <armirolaroseller@gmail.com>
2022-02-16 22:15:50 +0800

    Edited html, css, js files
    Co-authored-by: nehCnitsuJ <nehCnitsuJ@users.noreply.github.com>

---
+++ script/quiz.js
@@ -0,0 +1,5 @@
+//help popup when user presses help button on the top right of screen
+function helpFunction() {
+  var popup = document.querySelector(".help");
+  popup.classList.toggle("show");
+}
